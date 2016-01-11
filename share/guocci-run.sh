a6bc0f435bea7eec6d61cf0e731201334f49bad8
Boris Parak <parak@cesnet.cz>
2016-01-11 21:21:42 +0100

    Docker tweaks

--- share/guocci-run.sh
+++ share/guocci-run.sh
@@ -4,6 +4,15 @@
 #
 #
 
+# Location
 cd /opt/guocci
+
+# Services
 service memcached status || service memcached start
+
+# ENV
+export SECRET_KEY_BASE="f337c64a560bb7af70cfbf0d8e7d1b3fde4252a2ff4332bc94414dfd052797527f94eceafcbd11b0278320c97f1905ffaeabfe9b651e0c6119051079f4249274"
+export GUOCCI_VO_FILTER='fedcloud.egi.eu'
+
+# Start!
 bundle exec rails server -b 0.0.0.0 -e production
