aa4966a1e7a504ca2050e5e875340b7cab96f2dc
Boris Parak <parak@cesnet.cz>
2016-01-11 11:34:33 +0100

    Switching to Ubuntu 14.04

--- share/guocci-setup.sh
+++ share/guocci-setup.sh
@@ -5,7 +5,11 @@
 #
 
 apt-get update
-apt-get -q -y install ruby ruby-dev git devscripts autotools-dev build-essential libsqlite3-dev sqlite3 zlib1g-dev nodejs
+apt-get -q -y install ruby2.0 ruby2.0-dev git devscripts autotools-dev build-essential libsqlite3-dev sqlite3 zlib1g-dev nodejs
+
+rm /usr/bin/ruby && ln -s /usr/bin/ruby2.0 /usr/bin/ruby
+rm -fr /usr/bin/gem && ln -s /usr/bin/gem2.0 /usr/bin/gem
+
 gem install bundler
 
 git clone https://github.com/arax/guocci.git /opt/guocci
