e6e430b71dbc09dd549dcdf55c2fffa5a31e05d2
Boris Parak <parak@cesnet.cz>
2016-02-26 12:43:06 +0100

    Improved name detection from DN (no proxies)

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -2,6 +2,7 @@ class ProxyController < ApplicationController
   respond_to :json
 
   VOMS_PROXY_BIN = File.executable?('/usr/bin/voms-proxy-info') ? '/usr/bin/voms-proxy-info' : 'voms-proxy-info'
+  NUMERIC_PROXY_REGEXP = /^\d+$/
 
   def show
     respond_with(self.class.proxy_info(voms_proxy_path), status: 200)
@@ -27,9 +28,20 @@ class ProxyController < ApplicationController
       fail 'Failed to get proxy expiration time!' unless $?.to_i == 0
       proxy_info_h[:timeleft] = time_ago_in_words(Time.now + proxy_info_h[:timeleft].to_i)
 
-      proxy_info_h[:human] = proxy_info_h[:identity].split('/').last.split('=').last
+      proxy_info_h[:human] = name_candidate(proxy_info_h[:identity])
 
       proxy_info_h
+    end
+
+    def name_candidate(identity)
+      parts = identity.split('/').reject { |part| part.blank? }
+      parts.reverse.each do |candidate|
+        name = candidate.split('=').last
+        next if name.blank?
+        return name unless name =~ NUMERIC_PROXY_REGEXP
+      end
+
+      "Unknown"
     end
   end
 
