e96b379d3dfe10662cd1d83737baaacf5d2e4adb
Boris Parak <parak@cesnet.cz>
2016-06-08 14:41:09 +0200

    Changing proxy lookup (hashed)

--- app/controllers/concerns/voms_proxy_file.rb
+++ app/controllers/concerns/voms_proxy_file.rb
@@ -1,16 +1,22 @@
+require 'openssl'
+require 'base64'
+
 module VomsProxyFile
   def voms_proxy_path
-    Rails.logger.info "Selecting proxy: #{remote_proxy || InstancesController.voms_proxy_path}"
-    remote_proxy || InstancesController.voms_proxy_path
+    local_path = remote_proxy || InstancesController.voms_proxy_path
+    Rails.logger.info "Selecting proxy: #{local_path}"
+    local_path
   end
 
   def remote_proxy
-    remote_user_candid.blank? ? nil : "/tmp/x509_#{remote_user_candid}"
+    ruc = remote_user_candid
+    ruc.blank? ? nil : "/tmp/x509_#{ruc}"
   end
 
   def remote_user_candid
     Rails.logger.info "Remote user: #{ENV['REMOTE_USER'].inspect}"
     Rails.logger.info "Proxy user: #{request.headers['HTTP_PROXY_USER'].inspect}"
-    ENV['REMOTE_USER'] || request.headers['HTTP_PROXY_USER']
+    data = OpenSSL::Digest::SHA256.new.digest(ENV['REMOTE_USER'] || request.headers['HTTP_PROXY_USER'])
+    Base64.encode64(data).slice(0, 16)
   end
 end
